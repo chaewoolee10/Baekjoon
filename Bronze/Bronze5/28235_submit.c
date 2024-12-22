73a8f0ea05a26958158ce0e4f0ee7761f4ccaa24
chaewoolee10 <chaewoolee10@gmail.com>
2024-12-22 12:28:23 +0900

    https://www.acmicpc.net/submit/28235

---
+++ Bronze/Bronze5/28235_submit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+void sign(char *str);
+
+int main()
+{
+    char str[20] = {}; scanf("%s", str);
+    sign(str);
+        
+}
+void sign(char *str)
+{
+    if(!strcmp(str, "SONGDO"))
+    {
+        printf("HIGHSCHOOL");
+    }
+    else if(!strcmp(str, "CODE"))
+    {
+        printf("MASTER");
+    }
+    else if(!strcmp(str, "ALGORITHM"))
+    {
+        printf("CONTEST");
+    }
+    else
+    {
+        printf("0611");
+    }
+}
