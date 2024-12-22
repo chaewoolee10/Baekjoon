a3444782527f5cc8601c49552a17820713bb891a
chaewoolee10 <chaewoolee10@gmail.com>
2024-12-22 13:22:20 +0900

    https://www.acmicpc.net/problem/31450

---
+++ Bronze/Bronze5/31450_submit.c
@@ -0,0 +1,17 @@
+//31450
+#include <stdio.h>
+
+int main()
+{
+    int m; scanf("%d", &m);
+    int k; scanf("%d", &k);
+    
+    if(m % k == 0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
