2a0780887e92ab45d1970535da35b0eaf294e001
chaewoolee10 <chaewoolee10@gmail.com>
2025-01-22 10:44:19 +0900

    .

---
+++ Bronze/Bronze5/5339.c
@@ -0,0 +1,17 @@
+//5339
+#include <stdio.h>
+
+int main()
+{
+    printf("     /~\\\n");
+    printf("    ( oo|\n");
+    printf("    _\\=/_\n");
+    printf("   /  _  \\\n");
+    printf("  //|/.\\|\\\\\n");
+    printf(" ||  \\ /  ||\n");
+    printf("============\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    
+}
