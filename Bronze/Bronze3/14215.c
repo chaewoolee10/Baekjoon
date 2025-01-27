fe557b8102351761b36489b6db31c247ae3cf806
chaewoolee10 <chaewoolee10@gmail.com>
2025-01-27 09:55:28 +0900

    .

--- Bronze/Bronze3/14215.c
+++ Bronze/Bronze3/14215.c
@@ -1,4 +1,4 @@
-//14215
+// 14215
 #include <stdio.h>
 
 int main()
@@ -6,36 +6,30 @@ int main()
     int arr[3] = {};
     int max = 0;
     int maxindex = 0;
-    for(int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         scanf("%d", &arr[i]);
-        if(max < arr[i])
+        if (max < arr[i])
         {
             max = arr[i];
             maxindex = i;
         }
     }
     int remain = 0;
-    for(int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        if(i != maxindex)
+        if (i != maxindex)
         {
             remain += *(arr + i);
         }
     }
-    if(remain == max * 2) //정삼각형인 경우
+
+    if (remain <= max)
     {
-        printf("%d", max * 3);
+        printf("%d", 2 * remain - 1);
     }
-    else //정삼각형이 아닌 경우
+    else
     {
-        if(remain <= max)
-        {
-            printf("%d", 2 * remain - 1);
-        }
-        else
-        {
-            printf("%d", remain + max);
-        }
+        printf("%d", remain + max);
     }
 }
