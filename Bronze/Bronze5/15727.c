f965e7c4e300a28c5fb6c194947f55cd47c94f90
chaewoolee10 <chaewoolee10@gmail.com>
2025-01-30 10:09:36 +0900

    .

---
+++ Bronze/Bronze5/15727.c
@@ -0,0 +1,16 @@
+//15727
+#include <stdio.h>
+
+int main()
+{
+    int n; scanf("%d", &n);
+    if(n % 5 == 0)
+    {
+        printf("%d", n / 5);
+    }
+    else
+    {
+        printf("%d", n / 5 + 1);
+    }
+    
+}
