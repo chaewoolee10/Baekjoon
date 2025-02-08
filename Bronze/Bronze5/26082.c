258d00de4a60168facbbe2e6b1d482d3dd60aae8
chaewoolee10 <chaewoolee10@gmail.com>
2025-02-08 10:10:09 +0900

    .

---
+++ Bronze/Bronze5/26082.c
@@ -0,0 +1,13 @@
+//26082
+#include <stdio.h>
+
+int main()
+{
+    int a; scanf("%d", &a);
+    int b; scanf("%d", &b);
+    int c; scanf("%d", &c);
+    
+    printf("%d", 3 * b / a * c);
+    
+    //B / A == X / C
+}
