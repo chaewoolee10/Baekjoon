dc9e4ef67674f97693ecdf0eed1c409188841956
chaewoolee10 <chaewoolee10@gmail.com>
2025-02-18 10:08:46 +0900

    .

---
+++ Bronze/Bronze5/8370.c
@@ -0,0 +1,13 @@
+//8370
+#include <stdio.h>
+
+int main()
+{
+    int n1; scanf("%d", &n1);
+    int k1; scanf("%d", &k1);
+    int n2; scanf("%d", &n2);
+    int k2; scanf("%d", &k2);
+    
+    printf("%d", n1 * k1 + n2 * k2);
+    
+}
