cddc15f231a09d2b1229aac7ff4cfd85ef6c848d
chaewoolee10 <chaewoolee10@gmail.com>
2025-04-08 14:27:07 +0900

    .

---
+++ Silver/Silver5/1181_submit.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b);
+//int comparebydict(const void *a, const void *b);
+
+int main()
+{
+    char str[20000][21] = {};
+    
+    int n; scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%s", str[i]);
+    }
+    
+    qsort(str, n, sizeof(str[0]), compare);
+    
+    for(int i = 0; i < n; i++)
+    {
+        if(i < n - 1 && !strcmp(str[i], str[i + 1]))
+        {
+            continue;
+        }
+        else
+        {
+            printf("%s\n", str[i]);
+        }
+    }
+        
+}
+
+int compare(const void *a, const void *b)
+{
+    int len_a = strlen(a);
+    int len_b = strlen(b);
+    
+    if(len_a < len_b)
+    {
+        return -1;
+    }
+    else if(len_a > len_b)
+    {
+        return 1;
+    }
+    else
+    {
+        if(strcmp(a, b) > 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
