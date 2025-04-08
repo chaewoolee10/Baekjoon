1925688b165d7beb686bf0344b90a609a48d8b91
chaewoolee10 <chaewoolee10@gmail.com>
2025-04-08 10:31:01 +0900

    .

--- Silver/Silver5/1181_fail.c
+++ Silver/Silver5/1181_fail.c
@@ -1,57 +1,25 @@
-//1181
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b);
 
 int main()
 {
+    char str[20000][21] = {};
+    
     int n; scanf("%d", &n);
-    char str[20000][53] = {};
-    char tmp[53] = {};
-    int length[20000] = {};
-    int lengthtmp = 0;
     
     for(int i = 0; i < n; i++)
     {
-        scanf("%s", str[i]); //문자열 입력받기
-        for(int j = 0; j < i; j++)
-        {
-            if(!strcmp(str[i], str[j]))
-            {
-                str[i][0] = '\0';
-            }
-        }
-        if(str[i][0] != '\0')
-        {
-            length[i] = strlen(str[i]);
-        }
-        
-    }
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            if(strcmp(str[i], str[j]) < 0 && str[i][0] != '\0')
-            {
-                strcpy(tmp, str[i]);
-                strcpy(str[i], str[j]);
-                strcpy(str[j], tmp);
-                lengthtmp = length[i];
-                length[i] = length[j];
-                length[j] = lengthtmp;
-            }
-            
-        }
+        scanf("%s", str[i]);
     }
     
-    for(int i = 1; i <= 50; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            if(length[j] == i)
-            {
-                printf("%s\n", str[j]);
-            }
-        }
-    }
+    qsort(str, n, sizeof(char *), compare);
+}
+
+int compare(const void *a, const void *b)
+{
     
+    return strcmp(a, b);
 }
