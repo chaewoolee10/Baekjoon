7d51b1885fe63b49482c13c0cbe2fbb78ce17d0d
chaewoolee10 <chaewoolee10@gmail.com>
2025-04-17 10:36:45 +0900

    .

--- Bronze/Bronze1/1672_submit.c
+++ Bronze/Bronze1/1672_submit.c
@@ -14,32 +14,44 @@ int main()
     for(int i = 1; i < length; i++)
     {
         char c = checker(dna[length - i], dna[length - 1 - i]);
+        dna[length - 1 - i] = c;
+        dna[length - i] = '\0';
     }
-    
+    printf("%s", dna);
 }
 
 char checker(char a, char b) //a : n번째 ,  : n - 1번쨰
 {
+    char c;
     switch(a - b)
     {
         case 2:
         case -2:
-            return 'A';
+            c = 'A';
+            break;
         case 4:
         case -4:
-            return 'T';
+            c = 'T';
+            break;
         case 6:
         case -6:
-            return 'C';
-        case
+            c = 'C';
+            break;
+        case 17:
+        case -17:
+            c = 'G';
+            break;
+        case 13:
+        case -13:
+            c = 'A';
+            break;
+        case 19:
+        case -19:
+            c = 'G';
+            break;
+        default:
+            c = a;
+            break;
     }
-    
-    /*
-    T - A : 84 - 65 19
-    T - G ; 84 - 71 13
-    T - C : 84 - 67 17
-    G - A : 71 - 65 6
-    G - C : 71 - 67 4
-    C - A : 67 - 65 2
-    */
+    return c;
 }
