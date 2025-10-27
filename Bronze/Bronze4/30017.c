8302fbe92678c7420e27078c8f48f8005171f22c
chaewoolee10 <chaewoolee10@gmail.com>
2025-10-28 00:37:10 +0900

    .

---
+++ Bronze/Bronze4/30017.c
@@ -0,0 +1,19 @@
+//30017
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[1003] = {}; scanf("%s", str);
+    int length = strlen(str);
+    int count = 0;
+    for(int i = 0; i < length - 3; i++)
+    {
+        if(str[i] == 'D' && str[i + 1] == 'K' && str[i + 2] == 'S' && str[i + 3] == 'H')
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
+    
+}
