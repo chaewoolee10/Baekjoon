119172d2f571cf92d0ad78b468b58004dca1e7c6
chaewoolee10 <chaewoolee10@gmail.com>
2025-11-06 23:54:23 +0900

    .

---
+++ Bronze/Bronze4/31746.c
@@ -0,0 +1,16 @@
+//31746
+#include <stdio.h>
+
+int main()
+{
+    int n; scanf("%d", &n);
+    
+    if(n % 2 == 1)
+    {
+        printf("evoLmoCicS");
+    }
+    else
+    {
+        printf("SciComLove");
+    }
+}
