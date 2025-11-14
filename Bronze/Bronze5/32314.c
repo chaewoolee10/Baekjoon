687d2b68982fdc0826f11ccce3b9528b18a85b11
chaewoolee10 <chaewoolee10@gmail.com>
2025-11-15 00:21:59 +0900

    .

---
+++ Bronze/Bronze5/32314.c
@@ -0,0 +1,11 @@
+//32314
+#include <stdio.h>
+
+int main()
+{
+    int a; scanf("%d", &a);
+    int w; scanf("%d", &w);
+    int v; scanf("%d", &v);
+    
+    w >= a * v ? printf("1") : printf("0");
+}
