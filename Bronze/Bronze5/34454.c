02e5e742c82f2e8f32cc3f31204189153c35ab1c
chaewoolee10 <chaewoolee10@gmail.com>
2025-12-29 13:43:40 +0900

    .

---
+++ Bronze/Bronze5/34454.c
@@ -0,0 +1,11 @@
+//34454
+#include <stdio.h>
+
+int main()
+{
+    int n; scanf("%d", &n);
+    int c; scanf("%d", &c);
+    int p; scanf("%d", &p);
+    
+    c * p < n ? printf("no") : printf("yes");
+}
