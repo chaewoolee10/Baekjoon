748b3a5319a018336a9a7c044c0afeba23496d16
chaewoolee10 <chaewoolee10@gmail.com>
2026-01-23 09:23:16 +0900

    .

---
+++ Bronze/Bronze3/10995.c
@@ -0,0 +1,21 @@
+//10995
+#include <stdio.h>
+
+int main()
+{
+    int n; scanf("%d", &n);
+    
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 == 1)
+        {
+            printf(" ");
+        }
+        for(int j = 0; j < n; j++)
+        {
+            printf("*");
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
