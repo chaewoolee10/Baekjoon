6e866b93cdf8cb5656d843d112a93cce87364bb3
chaewoolee10 <chaewoolee10@gmail.com>
2026-02-09 17:39:33 +0900

    .

--- Bronze/Bronze1/1526_proceed.c
+++ Bronze/Bronze1/1526_proceed.c
@@ -1,81 +1,45 @@
-// 1526
+//1526
 #include <stdio.h>
-
-int checker(int *arr, int i);
 
 int main()
 {
-    int arr[7] = {};
-    int n;
-    scanf("%d", &n);
-    int i = 0;
-    while (1)
+    int n; scanf("%d", &n);
+    
+    while(1)
     {
-        if (n == 0)
+        int arr[7] = {};
+        int n_dup = n;
+        int i = 0;
+        while(1)
+        {
+            if(n_dup == 0)
+            {
+                break;
+            }
+            arr[i] = n_dup % 10;
+            n_dup /= 10;
+            i++;
+        }
+        int flag = 1;
+        for(int j = 0; j < i; j++)
+        {
+            if(arr[j] == 4 || arr[j] == 7)
+            {
+                continue;
+            }
+            else
+            {
+                flag = 0;
+            }
+        }
+        if(!flag)
+        {
+            n--;
+        }
+        else
         {
             break;
         }
-        arr[i] = n % 10;
-        n /= 10;
-        i++;
-    }
-
-    if (checker(arr, i) == 1)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (arr[j] > 7)
-            {
-                arr[j] = 7;
-            }
-            if (arr[j] < 4 && j != i - 1)
-            {
-                arr[j + 1]--;
-                arr[j] = 7;
-            }
-            if (arr[j] >= 4 && arr[j] < 7)
-            {
-                if (j != i - 1)
-                {
-                    arr[j] = 7;
-                    arr[j + 1]--;
-                }
-                else
-                {
-                    arr[j] = 4;
-                }
-            }
-            if (arr[i - 1] == 0)
-            {
-
-                break;
-            }
         }
-        if (arr[i - 1] <= 3 && arr[i - 1] > 0)
-        {
-            arr[i - 1] = 0;
-        }
-    }
-
-    for (int j = i - 1; j >= 0; j--)
-    {
-        if (arr[j] == 0)
-        {
-            continue;
-        }
-        printf("%d", arr[j]);
-    }
-}
-
-int checker(int *arr, int i)
-{
-    int check = 0;
-    for (int j = 0; j < i; j++)
-    {
-        if (arr[j] != 4 && arr[j] != 7)
-        {
-            check = 1;
-        }
-    }
-    return check;
+    printf("%d", n);
 }
