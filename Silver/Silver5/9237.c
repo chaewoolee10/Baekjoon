f9a0746d7092935da2442cb363cb0c1cc14a9d1c
chaewoolee10 <chaewoolee10@gmail.com>
2026-03-05 18:51:16 +0900

    .

---
+++ Silver/Silver5/9237.c
@@ -0,0 +1,40 @@
+//9237
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b);
+
+int main()
+{
+    int n; scanf("%d", &n);
+    int arr[100000] = {};
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, sizeof(arr[0]), compare);
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] += i + 1;
+    }
+    qsort(arr, n, sizeof(arr[0]), compare);
+    printf("%d", arr[0] + 1);
+}
+int compare(const void *a, const void *b)
+{
+    const int *pa = (const int *)a;
+    const int *pb = (const int *)b;
+    
+    if(*pa < *pb)
+    {
+        return 1;
+    }
+    else if(*pa > *pb)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
