b7f4da34b91302a96fde12a27ccaa79ff975b5eb
chaewoolee10 <chaewoolee10@gmail.com>
2026-03-27 10:37:48 +0900

    .

---
+++ Bronze/Bronze4/13985.c
@@ -0,0 +1,16 @@
+//13985
+#include <stdio.h>
+
+int main()
+{
+    
+    int a; scanf("%d", &a);
+    getchar();
+    getchar();
+    int b; scanf("%d", &b);
+    getchar();
+    getchar();
+    int res; scanf("%d", &res);
+    
+    a + b == res ? printf("YES") : printf("NO");
+}
